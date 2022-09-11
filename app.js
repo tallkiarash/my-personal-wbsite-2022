528b54db9a66e914dab38433c4e44b8e649def85
kiarash kiaeee <93583191+tallkiarash@users.noreply.github.com>
2022-09-11 15:49:07 +0200

    done the second page

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const { get } = require('express/lib/response')
+const data = require ('./data.js')
 const expressHandlebars = require ("express-handlebars")
 
 const app = express()
@@ -11,6 +12,12 @@ app.engine ('hbs', expressHandlebars.engine({
 
 app.get('/', function(request, response){
     response.render('start.hbs') // you can make the sprate file of HTML and specify the name to open here
+})
+
+app.get('/movies', function(request, response){
+    const model = {
+        movies: data.movies
+    }
 })
 
 //middleware function
