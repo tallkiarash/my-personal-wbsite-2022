fc6362e2f6c8024f0883d09916c346870f360db0
kiarash kiaeee <93583191+tallkiarash@users.noreply.github.com>
2022-09-11 14:35:16 +0200

    first page but has a problem

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const { get } = require('express/lib/response')
+const expressHandlebars = require ("express-handlebars")
+
+const app = express()
+
+//rendering engine
+app.engine ('hbs', expressHandlebars.engine({
+    defaultLayout: "main.hbs" , 
+}))
+
+app.get('/', function(request, response){
+    response.render('start.hbs') // you can make the sprate file of HTML and specify the name to open here
+})
+
+//middleware function
+app.use(
+    express.static('public')
+)
+
+app.listen(8080)
