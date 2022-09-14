05f055ce14cf74d68979b47ee5a27cb09373c502
kiarash kiaeee <93583191+tallkiarash@users.noreply.github.com>
2022-09-14 12:33:22 +0200

    all six stages added to the navbar

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require('express')
-const { get } = require('express/lib/response')
-const data = require ('./data.js')
-const expressHandlebars = require ("express-handlebars")
-
+const data = require('./data.js')
+const expressHandlebars = require("express-handlebars")
 const app = express()
 
 //rendering engine
-app.engine ('hbs', expressHandlebars.engine({
+app.engine('hbs', expressHandlebars.engine({
     defaultLayout: "main.hbs" , 
 }))
 
@@ -14,15 +12,47 @@ app.get('/', function(request, response){
     response.render('start.hbs') // you can make the sprate file of HTML and specify the name to open here
 })
 
-app.get('/movies', function(request, response){
+app.get('/portfolio', function(request, response){
     const model = {
-        movies: data.movies
+        portfolio: data.portfolio
     }
+    response.render('portfolio.hbs', model)
 })
 
+app.get('/aboutme', function (request, response){
+    const model = {
+        aboutme: data.aboutme
+    }
+
+    response.render('aboutme.hbs', model)
+})
+
+app.get('/information', function (request, response){
+    const model = {
+        information: data.information
+    }
+
+    response.render('information.hbs', model)
+})
+
+app.get('/blogPost', function (request, response){
+    const model = {
+        blogPost: data.blogPost
+    }
+
+    response.render('blogPost.hbs', model)
+})
+
+app.get('/guestBook', function (request, response){
+    const model = {
+        guestBook: data.guestBook
+    }
+
+    response.render('guestBook.hbs', model)
+})
 
 //get req for the movies one or two or...
-app.get ("/movies/:id", function(request, response){
+app.get("/movies/:id", function(request, response){
 
     const id = request.params.id
 
@@ -33,10 +63,10 @@ app.get ("/movies/:id", function(request, response){
     }
 
     response.render('movie.hbs', model)
-
 })
 
-//middleware function
+//middleware function 
+///send the file to the browser
 app.use(
     express.static('public')
 )
