5b66f4b7118e5649420f3c92726cc21eb5b234db
kiarash kiaeee <93583191+tallkiarash@users.noreply.github.com>
2022-10-23 16:02:58 +0200

    new

--- user.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require("bcrypt")
-
-
-userSchema.pre ('save', async function(next){
-    const salt = await bcrypt.genSalt;
-    this.actualpassword = await bcrypt.hash(this.actualpassword, salt);
-    next()
-})
